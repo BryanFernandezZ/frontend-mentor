1f65066fb46b4760ca192c1eaffe95b274ff41f9
Brayan Fernandez <brayanzabaleta59@gmail.com>
2022-09-16 06:22:25 -0500

    updated 16/09/2022

---
+++ chat-app-css-illustration-master/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- displays site properly based on user's device -->
+
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawesome.com/550ac71648.js" crossorigin="anonymous"></script>
+
+  <title>Frontend Mentor | [Challenge Name Here]</title>
+
+</head>
+
+<body>
+  <div class="element"></div>
+  <div class="element2"></div>
+
+  <section class="section_mobile">
+    <div class="mobile">
+
+      <div class="mobile_header">
+
+        <div class="camera"></div>
+        <div class="mobile_header_content">
+
+          <section>
+            <i class="fa-solid fa-chevron-left"></i>
+            <img src="images/avatar.jpg" alt="">
+            <div>Samuel Green <br>
+              <p>Available to Walk</p>
+            </div>
+          </section>
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+
+        </div>
+      </div>
+
+      <div class="mobile_content">
+
+        <div class="left_message">
+          That sounds great. I’d be happy with that.
+        </div>
+        <div class="left_message">
+          Could you send over some pictures of your dog, please?
+        </div>
+        <div class="right">
+          <div class="images">
+            <img src="images/dog-image-1.jpg" alt="">
+            <img src="images/dog-image-2.jpg" alt="">
+            <img src="images/dog-image-3.jpg" alt="">
+          </div>
+          <div class="right_message">
+            Here are a few pictures. She’s a happy girl!
+          </div>
+          <div class="right_message">
+            Can you make it?
+          </div>
+
+        </div>
+        <div class="left_message">
+          She looks so happy! The time we discussed works. How long shall I take her out for?
+        </div>
+
+        <div class="payment">
+          <section>
+            <i class="fa-regular fa-circle"></i>
+            <span>30 minute walk</span>
+          </section>
+          <h2>$29</h1>
+        </div>
+        <div class="payment">
+          <section>
+            <i class="fa-regular fa-circle"></i>
+            <span>1 hour walk</span>
+          </section>
+          <h2>$49</h2>
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Type a message..." />
+          <i class="fa-solid fa-chevron-right"></i>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="description">
+      <h2>Single booking</h2>
+      <p>Stay in touch with our dog walkers through the chat interface. This makes it easy to
+        discuss arrangements and make bookings. Once the walk has been completed you can rate
+        your walker and book again all through the chat.</p>
+    </div>
+  </section>
+
+  <div class="transparent"></div>
+
+</body>
+
+</html>
